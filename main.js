d75a40eef28281900448bb068eb08e7808def384
Franz Toledo <franz.wilber.tm@gmail.com>
2023-06-25 23:02:00 -0500

    edad errors

--- main.js
+++ main.js
@@ -6,8 +6,16 @@ const expresiones = {
 	nombre: /^[a-zA-ZÀ-ÿ\s]/, // Letras y espacios, pueden llevar acentos.
 	password: /^.{6,12}$/, // 5 a 12 digitos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{9}$/ //9 numeros.
+	telefono: /^\d{9}$/, //9 numeros.
+  edad: /^\d{1,3}$/
 };
+const campos = {
+	usuario: false,
+	nombre: false,
+	password: false,
+	correo: false,
+	telefono: false
+}
 const preventString= (e)=>{
   if (e.keyCode < 45 || e.keyCode > 57) {
     e.returnValue = false
@@ -31,18 +39,35 @@ const validarFormulario = (e)=>{
 		case "password2":
 			validarPassword2();
 		break;
-		case "correo":
-			validarCampo(expresiones.correo, e.target, 'correo');
+		case "email":
+			validarCampo(expresiones.correo, e.target, 'email');
 		break;
 		case "telefono":
 			validarCampo(expresiones.telefono, e.target, 'telefono');
 		break;
     case "edad":
-			validarCampo(expresiones.edad, e.target, 'edad');
+			validarCampo(expresiones.edad, e.target, 'edad')
 		break;
     case "website":
 			validarCampo(expresiones.website, e.target, 'website');
 		break;
+	}
+}
+const validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos[campo] = true;
+	} else {
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos[campo] = false;
 	}
 }
 formulario.addEventListener('submit', (e)=>{
