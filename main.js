12122d19289be7bdd502ec90ae8355f5524c28b2
Franz Toledo <franz.wilber.tm@gmail.com>
2023-06-25 19:37:24 -0500

    expresiones

---
+++ main.js
@@ -0,0 +1,7 @@
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{6,12}$/, // 5 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{9}$/ //9 numeros.
+}
