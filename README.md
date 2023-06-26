9b11302151dc9c806fdc2c68d9b73e20f46c8a65
Franz Toledo <franz.wilber.tm@gmail.com>
2023-06-26 04:37:54 -0500

    KONAMI code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FORM-JS
+
 Proyecto de formulario y validación con JavaScript
+
+CODIGO SECRETO DE KONAMI : "up", "up", "down", "down", "left", "right", "left", "right", "b", "a"
--- main.js
+++ main.js
@@ -149,3 +149,57 @@ inputSlider.oninput = (()=>{
 inputSlider.onblur = (()=>{
   slideValue.classList.remove("show");
 });
+// konami code
+
+let keys = {
+  37: "left",
+  38: "up",
+  39: "right",
+  40: "down",
+  65: "a",
+  66: "b"
+};
+
+let konamiCode = ["up", "up", "down", "down", "left", "right", "left", "right", "b", "a"];
+let keyCount = 0;
+let isOpen= false
+let timerID;
+
+document.addEventListener("keydown", checkCode, false);
+
+function checkCode(e) {
+  let keyPressed = keys[e.keyCode];
+
+  if (keyPressed === konamiCode[keyCount]) {
+    keyCount++;
+
+    // clear timer
+    window.clearTimeout(timerID);
+
+    // start timer with a 1 second delay before resetting state
+    timerID = window.setTimeout(resetKeyState, 4000);
+
+    // check if we are still on the right path
+    if (keyCount === konamiCode.length && !isOpen) {
+      cheatCodeActivated();
+			isOpen=true;
+      resetKeyState();
+    } else if (keyCount === konamiCode.length && isOpen){
+			cheatCodeDesactivated();
+			resetKeyState();
+		}
+  } else {
+    resetKeyState();
+  }
+}
+
+function cheatCodeActivated() {
+  modal_container.classList.add('show');
+}
+function cheatCodeDesactivated() {
+  modal_container.classList.remove('show');
+}
+function resetKeyState() {
+  window.clearTimeout(timerID);
+  keyCount = 0;
+}
