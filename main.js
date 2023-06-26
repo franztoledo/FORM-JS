f9274987bd58b36c746cb410d5867980bf57ec4f
Franz Toledo <franz.wilber.tm@gmail.com>
2023-06-25 20:49:42 -0500

    elementos y prevent default

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
 const expresiones = {
 	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
 	nombre: /^[a-zA-ZÀ-ÿ\s]/, // Letras y espacios, pueden llevar acentos.
 	password: /^.{6,12}$/, // 5 a 12 digitos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 	telefono: /^\d{9}$/ //9 numeros.
-}
+};
+formulario.addEventListener('submit', (e)=>{
+ e.preventDefault()
+ 
+});
